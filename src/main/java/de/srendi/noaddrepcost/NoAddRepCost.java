ca4aa779eb68566ed3763826dcc29fcb28a4beca
Sr_endi <info@denizdemiroglu.de>
2022-08-07 15:58:36 +0200

    Initial commit

---
+++ src/main/java/de/srendi/noaddrepcost/NoAddRepCost.java
@@ -0,0 +1,19 @@
+package de.srendi.noaddrepcost;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+@Mod(NoAddRepCost.MODID)
+public class NoAddRepCost {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MODID = "noaddrepcost";
+
+    public NoAddRepCost() {
+        LOGGER.info("No additional repair cost for you <3");
+    }
+
+}
