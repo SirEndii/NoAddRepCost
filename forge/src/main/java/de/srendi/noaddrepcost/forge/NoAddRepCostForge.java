a6bc612a077e61f24236eb211b28cb19320109a6
Srendi <info@denizdemiroglu.de>
2023-04-06 00:27:17 +0200

    port to 1.19.2

--- forge/src/main/java/de/srendi/noaddrepcost/forge/NoAddRepCostForge.java
+++ forge/src/main/java/de/srendi/noaddrepcost/forge/NoAddRepCostForge.java
@@ -1,15 +1,11 @@
 package de.srendi.noaddrepcost.forge;
 
-import dev.architectury.platform.forge.EventBuses;
 import de.srendi.noaddrepcost.NoAddRepCost;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(NoAddRepCost.MOD_ID)
 public class NoAddRepCostForge {
     public NoAddRepCostForge() {
-        // Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(NoAddRepCost.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         NoAddRepCost.init();
     }
 }
